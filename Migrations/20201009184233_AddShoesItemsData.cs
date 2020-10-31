d70e513d64a1fc3ba96f8d202cb610b63acd85d4
Danit Shagan <danishagan@gmail.com>
2020-10-31 13:49:34 +0200

    Add new Migration

--- Migrations/20201009184233_AddShoesItemsData.cs
+++ Migrations/20201009184233_AddShoesItemsData.cs
@@ -7,18 +7,18 @@ namespace DROD.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.InsertData(
-    table: "Items",
-    columns: new[] { "ItemName", "Price", "Gender", "Path" },
-    values: new object[,]
-    {
-                    { "S1", 89, 3, "../imgs/S1.JPEG"},
-                    { "S2", 99, 3, "../imgs/S2.JPEG"},
-                    { "S3", 79, 3, "../imgs/S3.JPEG"},
-                    { "S4", 99, 3, "../imgs/S4.JPEG"},
-                    { "S5", 109, 3, "../imgs/S5.JPEG"},
-                    { "S6", 69, 3, "../imgs/S6.JPEG"}
-    }
-);
+                table: "Items",
+                columns: new[] { "ItemName", "Price", "Gender", "Path" },
+                values: new object[,]
+                {
+                                { "S1", 89, 3, "../imgs/S1.JPEG"},
+                                { "S2", 99, 3, "../imgs/S2.JPEG"},
+                                { "S3", 79, 3, "../imgs/S3.JPEG"},
+                                { "S4", 99, 3, "../imgs/S4.JPEG"},
+                                { "S5", 109, 3, "../imgs/S5.JPEG"},
+                                { "S6", 69, 3, "../imgs/S6.JPEG"}
+                }
+            );
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
